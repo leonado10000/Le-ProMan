02c3026ca8a622d4c00276af92e7b0269924993c
Rahul Jangra <106389897+leonado10000@users.noreply.github.com>
2024-01-31 23:47:11 +0530

    streamlining

--- proto1/admin.py
+++ proto1/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+admin.site.register(models.TrackerUser)
+admin.site.register(models.Courses)
+admin.site.register(models.Topics)
+admin.site.register(models.CTtable)
+admin.site.register(models.Prograss)
