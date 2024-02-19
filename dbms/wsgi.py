896dcb5e02f2ed7d68089020cf58e88844feee91
Rahul Jangra <106389897+leonado10000@users.noreply.github.com>
2024-02-19 20:40:56 +0530

    refresh branch

--- dbms/wsgi.py
+++ dbms/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dbms.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dbms.settings")
 
+# application = get_wsgi_application()
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saveplate.settings')
 application = get_wsgi_application()
+app = application #add here
